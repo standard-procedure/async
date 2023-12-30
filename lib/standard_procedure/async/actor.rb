e4bb70be28f26e9cc970e3503d18a81b9d848dcd
Rahoul Baruah <rahoulb@echodek.co>
2023-12-30 10:04:57 +0000

    Tidied up implementation
    Adding initialize methods to modules can be tricky, so insteead we're creating the message queue and promises on demand.  This is safe because they are only ever called from within an actor thread and never by the outside world.

--- lib/standard_procedure/async/actor.rb
+++ lib/standard_procedure/async/actor.rb
@@ -5,68 +5,68 @@ require "concurrent/mvar"
 require "concurrent/immutable_struct"
 require_relative "promises"
 
-module StandardProcedure::Async
-  module Actor
-    def self.included base
-      base.class_eval do
-        extend ClassMethods
+module StandardProcedure
+  module Async
+    module Actor
+      def self.included base
+        base.class_eval do
+          def self.async name, &implementation
+            name = name.to_sym
+            implementation_name = :"_#{name}"
 
-        def initialize *args
-          super
-          @_promises = StandardProcedure::Async::Promises.new
-          @_messages = Concurrent::Array.new
+            define_method name do |*args, **params|
+              _add_message_to_queue(implementation_name, *args, **params)
+            end
+
+            define_method implementation_name do |*args, **params|
+              instance_exec(*args, **params, &implementation)
+            rescue => ex
+              ex
+            end
+          end
         end
       end
-    end
 
-    module ClassMethods
-      def async name, &implementation
-        name = name.to_sym
-        implementation_name = :"_#{name}"
+      private
 
-        define_method name do |*args, **params, &block|
-          _add_message_to_queue(implementation_name, *args, **params, &block)
-        end
-
-        define_method implementation_name do |*args, **params, &block|
-          implementation.call(*args, **params, &block)
-        end
+      def _messages
+        @_messages ||= Concurrent::Array.new
       end
-    end
 
-    private
+      def _promises
+        @_promises ||= StandardProcedure::Async::Promises.new
+      end
 
-    attr_reader :_messages
+      def _add_message_to_queue name, *args, **params, &block
+        message = Message.new(self, name, args, params, block, Concurrent::MVar.new)
+        _messages << message
+        _perform_messages if _messages.count == 1
+        message
+      end
 
-    def _add_message_to_queue name, *args, **params, &block
-      message = Message.new(self, name, args, params, block, Concurrent::MVar.new)
-      _messages << message
-      _perform_messages if _messages.count == 1
-      message
-    end
-
-    def _perform_messages
-      @_promises.future do
-        while (message = _messages.shift)
-          message.call
+      def _perform_messages
+        _promises.future do
+          while (message = _messages.shift)
+            message.call
+          end
         end
       end
-    end
 
-    # nodoc:
-    class Message < Concurrent::ImmutableStruct.new(:target, :name, :args, :params, :block, :result)
-      def value(timeout: 30)
-        result.take(timeout)
-      end
-      alias_method :get, :value
-      alias_method :await, :value
+      # nodoc:
+      class Message < Concurrent::ImmutableStruct.new(:target, :name, :args, :params, :block, :result)
+        def value(timeout: 30)
+          result.take(timeout)
+        end
+        alias_method :get, :value
+        alias_method :await, :value
 
-      def then &block
-        block&.call value
-      end
+        def then &block
+          block&.call value
+        end
 
-      def call
-        result.put target.send(name, *args, **params, &block)
+        def call
+          result.put target.send(name, *args, **params, &block)
+        end
       end
     end
   end
